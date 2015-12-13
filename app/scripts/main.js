24d022efe88926e84fe64a458a86fb04addeadb1
Laith Shadeed <laith.shadeed@gmail.com>
2015-12-13 22:50:08 +0100

    Add some style

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,6 @@
 
   angular.module('wskFeedback', ['firebase'])
   .controller('FeedbackController', function($scope, $firebaseObject) {
-    console.log('hii');
     var ref = new Firebase('https://wsk-feedback.firebaseio.com');
     $scope.data = $firebaseObject(ref);
   });
